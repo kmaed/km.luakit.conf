7992fc90d19864866828b5ef9d69437ff3412469
Kazuki Maeda <kmaeda@kmaeda.net>
2019-01-05 22:37:31 +0900

    Update C-g, C-v, M-v, B -> b, F -> f, and use m and M to enter follow mode.

--- userconf.lua
+++ userconf.lua
@@ -41,14 +41,6 @@ local actions = { scroll = {
         desc = "Scroll the current page right.",
         func = function (w, m) w:scroll{ xrel =  settings.get_setting("window.scroll_step")*(m.count or 1) } end,
     },
-    page_up = {
-        desc = "Scroll the current page up a full screen.",
-        func = function (w, m) w:scroll{ ypagerel = -(m.count or 1) } end,
-    },
-    page_down = {
-        desc = "Scroll the current page down a full screen.",
-        func = function (w, m) w:scroll{ ypagerel =  (m.count or 1) } end,
-    },
 }, zoom = {
     zoom_in = {
         desc = "Zoom in to the current page.",
@@ -78,10 +70,15 @@ modes.add_binds("normal", {
                    {"<Control-n>", actions.scroll.down},
                    {"<Control-f>", actions.scroll.left},
                    {"<Control-b>", actions.scroll.right},
-                   {"<Mod1-v>", actions.scroll.page_up},
-                   {"<Control-v>", actions.scroll.page_down},
+                   {"<Control-g>", "Stop loading.",
+                    function (w) w.view:stop() end },
+                   {"<Control-v>", "Scroll the current page down",
+                    function (w) w:scroll{ ypagerel =  0.95 } end },
+                   {"<Mod1-v>", "Scroll the current page up",
+                    function (w) w:scroll{ ypagerel =  -0.95 } end },
                    {"<Control-Mod1-r>", "Reload userconf.lua.",
-                    function(w) dofile(lousy.util.find_config("userconf.lua")); w:enter_cmd(":"); w:set_mode() end},
+                    function(w) dofile(lousy.util.find_config("userconf.lua"))
+                       w:enter_cmd(":"); w:set_mode() end},
                    {"0", actions.zoom.zoom_set},
                    {"=", actions.zoom.zoom_in},
                    {"o", "Open one or more URLs.",
@@ -92,10 +89,30 @@ modes.add_binds("normal", {
                      function (w) clearhistory(); w:enter_cmd(":open " .. (w.view.uri or "")) end },
                    {"T", "Open one or more URLs based on current location in a new tab.",
                      function (w) clearhistory(); w:enter_cmd(":tabopen " .. (w.view.uri or "")) end },
-                   {"B", "Go back in the browser history `[count=1]` items.",
+                   {"b", "Go back in the browser history `[count=1]` items.",
                     function (w, m) w:back(m.count) end },
-                   {"F", "Go forward in the browser history `[count=1]` times.",
+                   {"f", "Go forward in the browser history `[count=1]` times.",
                     function (w, m) w:forward(m.count) end },
+                   {"^m$", [[Start `follow` mode. Hint all clickable elements
+                            (as defined by the `follow.selectors.clickable`
+                             selector) and open links in the current tab.]],
+                    function (w)
+                       w:set_mode("follow", {
+                                     selector = "clickable", evaluator = "click",
+                                     func = function (s) w:emit_form_root_active_signal(s) end,
+                       })
+                   end },
+                   {"^M$", [[Start follow mode. Hint all links (as defined by the
+                              `follow.selectors.uri` selector) and open links in a new tab.]],
+                     function (w)
+                        w:set_mode("follow", {
+                                      prompt = "background tab", selector = "uri", evaluator = "uri",
+                                      func = function (uri)
+                                         assert(type(uri) == "string")
+                                         w:new_tab(uri, { switch = false, private = w.view.private })
+                                      end
+                        })
+                   end },
 })
 modes.add_binds("passthrough", {
                    { "<Control-g>", "Return to `normal` mode.",
