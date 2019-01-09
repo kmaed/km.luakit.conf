8cccf07100f8aff8d2afe7ec50cdf419760e6661
Kazuki Maeda <kmaeda@kmaeda.net>
2019-01-09 23:49:07 +0900

    indent.

--- userconf.lua
+++ userconf.lua
@@ -19,8 +19,8 @@ local follow = require("follow")
 local select = require("select")
 -- hint label
 select.label_maker = function ()
-    local chars = charset("asdfghjkl")
-    return trim(sort(reverse(chars)))
+   local chars = charset("asdfghjkl")
+   return trim(sort(reverse(chars)))
 end
 -- Match only hint label text
 follow.pattern_maker = follow.pattern_styles.match_label
@@ -69,39 +69,39 @@ modes.remove_binds("normal", {"j", "k", "h", "l", "^", "$", "i", "w", "M", "%",
 modes.remove_binds("insert", {"<Control-e>", "<Control-z>"})
 -- from binds.lua
 local actions = { scroll = {
-    up = {
-        desc = "Scroll the current page up.",
-        func = function (w, m) w:scroll{ yrel = -settings.get_setting("window.scroll_step")*(m.count or 1) } end,
-    },
-    down = {
-        desc = "Scroll the current page down.",
-        func = function (w, m) w:scroll{ yrel =  settings.get_setting("window.scroll_step")*(m.count or 1) } end,
-    },
-    left = {
-        desc = "Scroll the current page left.",
-        func = function (w, m) w:scroll{ xrel = -settings.get_setting("window.scroll_step")*(m.count or 1) } end,
-    },
-    right = {
-        desc = "Scroll the current page right.",
-        func = function (w, m) w:scroll{ xrel =  settings.get_setting("window.scroll_step")*(m.count or 1) } end,
-    },
+                     up = {
+                        desc = "Scroll the current page up.",
+                        func = function (w, m) w:scroll{ yrel = -settings.get_setting("window.scroll_step")*(m.count or 1) } end,
+                     },
+                     down = {
+                        desc = "Scroll the current page down.",
+                        func = function (w, m) w:scroll{ yrel =  settings.get_setting("window.scroll_step")*(m.count or 1) } end,
+                     },
+                     left = {
+                        desc = "Scroll the current page left.",
+                        func = function (w, m) w:scroll{ xrel = -settings.get_setting("window.scroll_step")*(m.count or 1) } end,
+                     },
+                     right = {
+                        desc = "Scroll the current page right.",
+                        func = function (w, m) w:scroll{ xrel =  settings.get_setting("window.scroll_step")*(m.count or 1) } end,
+                     },
 }, zoom = {
-    zoom_in = {
-        desc = "Zoom in to the current page.",
-        func = function (w, m) w:zoom_in(settings.get_setting("window.zoom_step") * (m.count or 1)) end,
-    },
-    zoom_out = {
-        desc = "Zoom out from the current page.",
-        func = function (w, m) w:zoom_out(settings.get_setting("window.zoom_step") * (m.count or 1)) end,
-    },
-    zoom_set = {
-        desc = "Zoom to a specific percentage when specifying a count, and reset the page zoom otherwise.",
-        func = function (w, m)
-            local zoom_level = m.count or settings.get_setting_for_view(w.view, "webview.zoom_level")
-            w:zoom_set(zoom_level/100)
-        end,
-    },
-}}
+                     zoom_in = {
+                        desc = "Zoom in to the current page.",
+                        func = function (w, m) w:zoom_in(settings.get_setting("window.zoom_step") * (m.count or 1)) end,
+                     },
+                     zoom_out = {
+                        desc = "Zoom out from the current page.",
+                        func = function (w, m) w:zoom_out(settings.get_setting("window.zoom_step") * (m.count or 1)) end,
+                     },
+                     zoom_set = {
+                        desc = "Zoom to a specific percentage when specifying a count, and reset the page zoom otherwise.",
+                        func = function (w, m)
+                           local zoom_level = m.count or settings.get_setting_for_view(w.view, "webview.zoom_level")
+                           w:zoom_set(zoom_level/100)
+                        end,
+                     },
+                }}
 modes.add_binds("all", {
                    { "<Control-g>", "Return to `normal` mode.",
                      function (w)
@@ -121,9 +121,9 @@ modes.add_binds("normal", {
                    {"<Mod1-v>", "Scroll the current page up",
                     function (w) w:scroll{ ypagerel =  -0.95 } end },
                    { "<space>", "Scroll the current page down",
-                    function (w) w:scroll{ ypagerel =  0.95 } end },
+                     function (w) w:scroll{ ypagerel =  0.95 } end },
                    { "<Shift-space>", "Scroll the current page up",
-                    function (w) w:scroll{ ypagerel =  -0.95 } end },
+                     function (w) w:scroll{ ypagerel =  -0.95 } end },
                    {"<Control-Mod1-r>", "Restart luakit (reloading configs).",
                     function (w) w:restart() end },
                    {"<Control-q>", "Enter `passthrough` mode, ignores all luakit keybindings.",
@@ -153,23 +153,23 @@ modes.add_binds("normal", {
                    end },
                    {"^M$", [[Start follow mode. Hint all links (as defined by the
                               `follow.selectors.uri` selector) and open links in a new tab.]],
-                     function (w)
-                        w:set_mode("follow", {
-                                      prompt = "background tab", selector = "uri", evaluator = "uri",
-                                      func = function (uri)
-                                         assert(type(uri) == "string")
-                                         w:new_tab(uri, { switch = false, private = w.view.private })
-                                      end
-                        })
+                    function (w)
+                       w:set_mode("follow", {
+                                     prompt = "background tab", selector = "uri", evaluator = "uri",
+                                     func = function (uri)
+                                        assert(type(uri) == "string")
+                                        w:new_tab(uri, { switch = false, private = w.view.private })
+                                     end
+                       })
                    end },
 })
 modes.add_binds("completion", {
-    { "<Control-n>", "Select next matching completion item.",
-      function (w) w.menu:move_down() end },
-    { "<Control-p>", "Select previous matching completion item.",
-      function (w) w.menu:move_up() end },
-    { "<Control-g>", "Stop completion and restore original command.",
-      completion.exit_completion },
+                   { "<Control-n>", "Select next matching completion item.",
+                     function (w) w.menu:move_down() end },
+                   { "<Control-p>", "Select previous matching completion item.",
+                     function (w) w.menu:move_up() end },
+                   { "<Control-g>", "Stop completion and restore original command.",
+                     completion.exit_completion },
 })
 modes.add_binds("insert", {
                    { "<Control-q>", "Enter `passthrough` mode, ignores all luakit keybindings.",
